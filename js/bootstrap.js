7c05a1b751e05acb7fa0c4179985f8fff7f90890
Dhruv <98452140+DHRUVDC@users.noreply.github.com>
2024-01-08 11:28:42 +0530

    Pointrix

---
+++ js/bootstrap.js
@@ -0,0 +1,27 @@
+import * as Popper from "@popperjs/core/dist/umd/popper.js";
+import Alert from "bootstrap/js/dist/alert";
+import Button from "bootstrap/js/dist/button";
+import Carousel from "bootstrap/js/dist/carousel";
+import Collapse from "bootstrap/js/dist/collapse";
+import Dropdown from "bootstrap/js/dist/dropdown";
+import Modal from "bootstrap/js/dist/modal";
+import Offcanvas from "bootstrap/js/dist/offcanvas";
+import Popover from "bootstrap/js/dist/popover";
+import Scrollspy from "bootstrap/js/dist/scrollspy";
+import Tab from "bootstrap/js/dist/tab";
+import Toast from "bootstrap/js/dist/toast";
+import Tooltip from "bootstrap/js/dist/tooltip";
+export default {
+  Alert: Alert,
+  Button: Button,
+  Carousel: Carousel,
+  Collapse: Collapse,
+  Dropdown: Dropdown,
+  Modal: Modal,
+  Offcanvas: Offcanvas,
+  Popover: Popover,
+  Scrollspy: Scrollspy,
+  Tab: Tab,
+  Toast: Toast,
+  Tooltip: Tooltip,
+};
